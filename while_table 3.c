c6c397ebff9120205827c8d49aaa5b25cbf8565e
dhavalbhatti <engineer.dhavalbhatti@gmail.com>
2023-09-06 12:54:38 +0530

    Latest

---
+++ while_table 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int i, num;
+    i = 1;
+    printf("Please enter a number = ");
+    scanf("%d", &num);
+    printf("Table of %d\n", num);
+    while (i <= 10 /* condition */)
+    {
+       printf("%d x %d = %d \n", num,i, num*i);
+       i++; /* code */
+    }
+    return 0;
+}
